23289517c2406aebd83cd5be4790131a2213402d
r51 <darshitmendapara1425@gmail.com>
2023-02-06 23:05:21 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+import 'Page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    routes: {'/': (context) => P1()},
+  ));
+}
